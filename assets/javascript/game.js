b64a3916146e25e93039f1cf5eb1ad086b174641
EliteAngelus <Angeluselite@gmail.com>
2017-12-02 21:25:57 -0500

    added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,14 @@
+
+// VARIABLES SET TO 0
 var numbersum = 0;
 var matchNum = 0;
 var wins = 0;
 var losses = 0;
 
-$("#resetButton").on('click', function(){
-	matchNum = Math.floor(Math.random() * 120 + 19) ;
-	$("#numToMatch").html(matchNum);
-	$("#resetButton").hide();
-	$("#score-count").html(" ");
-	$(".crystal-images").show()
-	numbersum = 0;
-	redCrys = Math.floor(Math.random() * 12) + 1;
-	yelCrys =  Math.floor(Math.random() * 12) + 1;
-	pinCrys =  Math.floor(Math.random() * 12) + 1;
-	purCrys =  Math.floor(Math.random() * 12) + 1;		
-	console.log("works");
-})	
-
+// IF ELSE STATEMENT STATING CONDITIONS FOR WINS OR LOSSES
 var logic = function(){
+	// IF THE NUMBER IS EQUAL TO THE NUMBER GENERATED. ALERT THE USER
+	// SHOW THE RESET BUTTON, HIDE THE CRYSTAL BUTTONS, INCREASES THE COUNTER.
 	if (numbersum === matchNum ) {
 		wins++;
 	console.log(wins);
@@ -26,7 +17,9 @@ var logic = function(){
 	$(".crystal-images").hide()
 	$("#results").append("<p>Wins: " + wins + "</p>");
 	
-}
+}	// IF THE NUMBER IS MORE TO THE NUMBER GENERATED. ALERT THE USER
+	// SHOW THE RESET BUTTON, HIDE THE CRYSTAL BUTTONS, INCREASES THE COUNTER.
+	
 	else if(numbersum > matchNum){
 		losses++;
 		console.log(losses);
@@ -38,7 +31,10 @@ var logic = function(){
 	}	
 	
 }
-		
+
+//THE GAMES FUNCTIONS: TELLS THE GAME TO FIRST HIDE THE RESET BUTTON. ASSIGNS RANDOM NUMBERS
+ // TO THE CRYSTALS, WITH AN ON CLICK FUNCTION FOR EACH CRYSTAL.  
+
 function game(){
 
 $("#resetButton").hide();
@@ -80,5 +76,26 @@ console.log(numbersum);
 	});
 	
 }
+// THE RESET BUTTON, 
+// 1. WILL GENERATE A NEW RANDOM NUMBER TO BE MATCH, 
+// 2. WILL HIDE THE RESET BUTTON UNTIL THE IF/ELSE STATMENT SHOWS IT AGAIN, 
+// 3. EMPTIES THE SCORE-COUNT DIV, 
+// 4. SHOWS THE CRYSTAL IMAGES THAT HIDE WHEN THE GAME IS COMPLETED. 
+// 5. GENERATES NEW RANDOM NUMBERS FOR THE CRYSTAL IMAGES
+// 6. RETURNS THE SUM OF THE CRYSTAL NUMBERS (numberSum) BACK TO 0.
+
+$("#resetButton").on('click', function(){
+	matchNum = Math.floor(Math.random() * 120 + 19) ;
+	$("#numToMatch").html(matchNum);
+	$("#resetButton").hide();
+	$("#score-count").html(" ");
+	$(".crystal-images").show()
+	redCrys = Math.floor(Math.random() * 12) + 1;
+	yelCrys =  Math.floor(Math.random() * 12) + 1;
+	pinCrys =  Math.floor(Math.random() * 12) + 1;
+	purCrys =  Math.floor(Math.random() * 12) + 1;		
+	console.log("works");
+	numbersum = 0;
+})	
 
 game();
