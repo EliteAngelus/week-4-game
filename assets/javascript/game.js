4534310c334509d03d899be9753d94b2be170f10
EliteAngelus <Angeluselite@gmail.com>
2018-05-09 03:52:13 -0400

    Edited CSS, added sound effects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 // VARIABLES SET TO 0
 var numbersum = 0;
 var matchNum = 0;
@@ -6,87 +5,85 @@ var wins = 0;
 var losses = 0;
 
 // IF ELSE STATEMENT STATING CONDITIONS FOR WINS OR LOSSES
-var logic = function(){
-	// IF THE NUMBER IS EQUAL TO THE NUMBER GENERATED. ALERT THE USER
-	// SHOW THE RESET BUTTON, HIDE THE CRYSTAL BUTTONS, INCREASES THE COUNTER.
-	if (numbersum === matchNum ) {
-		wins++;
-	console.log(wins);
-	alert("You win!")
-	$("#resetButton").show()
-	$(".crystal-images").hide()
-	$("#win").html("<p>Wins: " + wins + "</p>");
-	
-}	// IF THE NUMBER IS MORE TO THE NUMBER GENERATED. ALERT THE USER
-	// SHOW THE RESET BUTTON, HIDE THE CRYSTAL BUTTONS, INCREASES THE COUNTER.
-	
-	else if(numbersum > matchNum){
-		losses++;
-		console.log(losses);
-		alert("You lose!")
-		$("#resetButton").show()
-		$(".crystal-images").hide()
-		$("#lose").html("<p>Losses: " + losses + "</p>");
-	
-	}	
+var logic = function() {
+    // IF THE NUMBER IS EQUAL TO THE NUMBER GENERATED. ALERT THE USER
+    // SHOW THE RESET BUTTON, HIDE THE CRYSTAL BUTTONS, INCREASES THE COUNTER.
+    if (numbersum === matchNum) {
+        document.getElementById('correct').play();
+        wins++;
+        console.log(wins);
+        alert("You win!")
+        $("#resetButton").show()
+        $(".pulse").hide()
+        $("#win").html("<p>Wins: " + wins + "</p>");
 
-	else if ( losses > 4){
-		alert("You Lose! GAME OVER")
+    } // IF THE NUMBER IS MORE TO THE NUMBER GENERATED. ALERT THE USER
+    // SHOW THE RESET BUTTON, HIDE THE CRYSTAL BUTTONS, INCREASES THE COUNTER.
+    else if (numbersum > matchNum) {
+        document.getElementById('wrong').play();
+        losses++;
+        console.log(losses);
+        alert("You lose!")
+        $("#resetButton").show()
+        $(".pulse").hide()
+        $("#lose").html("<p>Losses: " + losses + "</p>");
 
-		location.reload();
-	}
+    } else if (losses > 4) {
+        alert("You Lose! GAME OVER")
+        document.getElementById('zelda').pause();
+        document.getElementById('gameOver').play();
+        location.reload();
+    } else if (win > 4) {
+        alert("You Win! GAME OVER")
+        location.reload();
+    }
 
-	else if(win > 4){
-		alert("You Win! GAME OVER")
-		location.reload();
-	}
-	
 }
 
 //THE GAMES FUNCTIONS: TELLS THE GAME TO FIRST HIDE THE RESET BUTTON. ASSIGNS RANDOM NUMBERS
- // TO THE CRYSTALS, WITH AN ON CLICK FUNCTION FOR EACH CRYSTAL.  
+// TO THE CRYSTALS, WITH AN ON CLICK FUNCTION FOR EACH CRYSTAL.  
 
-function game(){
+function game() {
 
-$("#gameOver").hide();
-$("#resetButton").hide();
+    $("#gameOver").hide();
+    $("#resetButton").hide();
 
-redCrys = Math.floor(Math.random() * 12) + 1;
-yelCrys =  Math.floor(Math.random() * 12) + 1;
-pinCrys =  Math.floor(Math.random() * 12) + 1;
-purCrys =  Math.floor(Math.random() * 12) + 1;					
+    redCrys = Math.floor(Math.random() * 12) + 1;
+    yelCrys = Math.floor(Math.random() * 12) + 1;
+    pinCrys = Math.floor(Math.random() * 12) + 1;
+    purCrys = Math.floor(Math.random() * 12) + 1;
 
-console.log(numbersum);
+    console.log(numbersum);
 
-	matchNum = Math.floor(Math.random() * 120 +19) ;
-	$("#numToMatch").html(matchNum);
+    matchNum = Math.floor(Math.random() * 120 + 19);
+    $(".resultsDigits").html(matchNum);
 
-	$('#red').on('click', function(){
-  	numbersum += redCrys;
+    $('#red').on('click', function() {
+        numbersum += redCrys;
 
-	 $('#score-count').html(numbersum);
+        $('.countDigits').html(numbersum);
 
-	 logic();	
-	});
+        logic();
+    });
 
-	$('#yellow').on('click', function(){
-	  	numbersum += yelCrys;
-	 $('#score-count').html(numbersum);
-	  logic();	
-	  console.log(yelCrys);
-	});
-	$('#pink').on('click', function(){
-	  	numbersum += pinCrys;
-	 $('#score-count').html(numbersum);
-	 logic();	
+    $('#yellow').on('click', function() {
+        numbersum += yelCrys;
+        $('.countDigits').html(numbersum);
+        logic();
+        console.log(yelCrys);
+    });
+    $('#pink').on('click', function() {
+        numbersum += pinCrys;
+        $('.countDigits').html(numbersum);
+        logic();
 
-	});
-	$('#purple').on('click', function(){
-	  	var total = numbersum += purCrys;
-	 $('#score-count').html(total);
-	 logic();	
-	});
-	
+    });
+    $('#purple').on('click', function() {
+        var total = numbersum += purCrys;
+        $('.countDigits').html(total);
+        logic();
+    });
+
 }
 // THE RESET BUTTON, 
 // 1. WILL GENERATE A NEW RANDOM NUMBER TO BE MATCH, 
@@ -96,19 +93,19 @@ console.log(numbersum);
 // 5. GENERATES NEW RANDOM NUMBERS FOR THE CRYSTAL IMAGES
 // 6. RETURNS THE SUM OF THE CRYSTAL NUMBERS (numberSum) BACK TO 0.
 
-$("#resetButton").on('click', function(){
-	matchNum = Math.floor(Math.random() * 120 + 19) ;
-	$("#numToMatch").html(matchNum);
-	$("#resetButton").hide();
-	$("#score-count").html(" ");
-	$(".crystal-images").show()
-	redCrys = Math.floor(Math.random() * 12) + 1;
-	yelCrys =  Math.floor(Math.random() * 12) + 1;
-	pinCrys =  Math.floor(Math.random() * 12) + 1;
-	purCrys =  Math.floor(Math.random() * 12) + 1;		
-	console.log("works");
-	numbersum = 0;
-})	
+$("#resetButton").on('click', function() {
+    matchNum = Math.floor(Math.random() * 120 + 19);
+    $(".resultsDigits").html(matchNum);
+    $("#resetButton").hide();
+    $(".countDigits").html(" ");
+    $(".pulse").show()
+    redCrys = Math.floor(Math.random() * 12) + 1;
+    yelCrys = Math.floor(Math.random() * 12) + 1;
+    pinCrys = Math.floor(Math.random() * 12) + 1;
+    purCrys = Math.floor(Math.random() * 12) + 1;
+    console.log("works");
+    numbersum = 0;
+})
 
 
 game();
